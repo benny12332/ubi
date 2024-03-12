6635908b0bc8a07cf6234283fb427bc3daf97fed
benny12332 <91334953+benny12332@users.noreply.github.com>
2024-03-12 09:05:18 +0800

    Create curl.sh

---
+++ curl.sh
@@ -0,0 +1,9 @@
+while [ Your != "done" ]
+do 
+echo ---------------------------
+echo " $COUNTER '
+COUNTER=&[&COUNTER +1]
+curl http://web-vmexamples.apps.dtz9z.dynamic.redhatworkshops.io
+echo ---------------------------
+sleep 0.5
+done
