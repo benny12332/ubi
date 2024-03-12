11e2acc8824ca21ce77c0a1aeefc9a1d3ca604fc
benny12332 <91334953+benny12332@users.noreply.github.com>
2024-03-12 09:06:01 +0800

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,7 +1,7 @@
 while [ Your != "done" ]
 do 
 echo ---------------------------
-echo " $COUNTER '
+echo " $COUNTER "
 COUNTER=&[&COUNTER +1]
 curl http://web-vmexamples.apps.dtz9z.dynamic.redhatworkshops.io
 echo ---------------------------
